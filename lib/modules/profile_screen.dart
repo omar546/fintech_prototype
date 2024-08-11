c85867ddacf76016d6ec54b39c89bc91311301c2
AnoPiS <omer.mkawy.om@gmail.com>
2024-08-11 22:44:30 +0300

    cards data

--- lib/modules/profile_screen.dart
+++ lib/modules/profile_screen.dart
@@ -9,11 +9,14 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Styles.greyColor,
-      body: Center(
-          child: Column(
-        children: [
-          const Spacer(),
-          Stack(children: [
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+        Padding(
+          padding: const EdgeInsets.only(top:80.0),
+          child: Stack(children: [
             ClipOval(
               child: Image.asset(
                 'assets/avatar_147133.png',
@@ -37,78 +40,79 @@ class ProfileScreen extends StatelessWidget {
                       onPressed: () {},
                     ))),
           ]),
-          const SizedBox(
-            height: 10,
-          ),
-          const Text(
-            'Client Name',
-            style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-          ),
-          const Text(
-            'ClientName@gmail.com',
-            style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    buildProfileButton(
-                        action: () {}, icon: Icons.settings, text: 'Settings'),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    buildProfileButton(
-                        action: () {}, icon: Icons.dark_mode, text: 'Theme'),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    buildProfileButton(
-                        action: () {},
-                        icon: Icons.credit_card_outlined,
-                        text: 'Your Cards'),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    buildProfileButton(
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const Text(
+          'Client Name',
+          style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+        ),
+        const Text(
+          'ClientName@gmail.com',
+          style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  buildProfileButton(
+                      action: () {}, icon: Icons.settings, text: 'Settings'),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  buildProfileButton(
+                      action: () {}, icon: Icons.dark_mode, text: 'Theme'),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  buildProfileButton(
                       action: () {},
-                      icon: Icons.laptop,
-                      text: 'Your Fees',
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    buildProfileButton(
-                        action: () {},
-                        icon: Icons.support_agent_rounded,
-                        text: 'Contact Us'),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    buildProfileButton(
-                        action: () {},
-                        icon: Icons.logout,
-                        text: 'Logout',
-                        iconColor: Colors.red.withAlpha(200)),
-                  ],
-                ),
-              ],
-            ),
+                      icon: Icons.credit_card_outlined,
+                      text: 'Your Cards'),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  buildProfileButton(
+                    action: () {},
+                    icon: Icons.laptop,
+                    text: 'Your Fees',
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  buildProfileButton(
+                      action: () {},
+                      icon: Icons.support_agent_rounded,
+                      text: 'Contact Us'),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  buildProfileButton(
+                      action: () {},
+                      icon: Icons.logout,
+                      text: 'Logout',
+                      iconColor: Colors.red.withAlpha(200)),
+                ],
+              ),
+            ],
           ),
-          const Spacer(),
-        ],
-      )),
+        ),
+                ],
+              ),
+      ),
     );
   }
 }
